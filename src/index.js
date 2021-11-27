fdbf36d632d61022b650ed31fc28c4c03bb3852c
Harsh Ghodkar <harshghodkar@gmail.com>
2021-11-27 23:25:14 +0530

    second

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import App from "./App";
 import { MoralisProvider } from "react-moralis";
 import "./index.css";
 import QuickStart from "components/QuickStart";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 /** Get your free Moralis Account https://moralis.io/ */
 
-const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+const APP_ID = "bF1Wa2B59a0vgGAGtxpOE6NzHimrmePa8wPpsyAN";
+const SERVER_URL = "https://d1qv5atvepad.usemoralis.com:2053/server";
 
 const Application = () => {
   const isServerInfo = APP_ID && SERVER_URL ? true : false;
   if (isServerInfo)
     return (
-      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <App isServerInfo />
-      </MoralisProvider>
+      <React.StrictMode>
+        <HashRouter>
+          <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+            <App isServerInfo />
+          </MoralisProvider>
+        </HashRouter>
+      </React.StrictMode>
     );
   else {
     return (
