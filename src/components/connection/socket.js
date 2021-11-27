efe593473be93e7714f81db0fccc0b6f8e9b2991
Harsh Ghodkar <harshghodkar@gmail.com>
2021-11-27 22:55:39 +0530

    first

---
+++ src/components/connection/socket.js
@@ -0,0 +1,22 @@
+import io from "socket.io-client";
+
+const URL = "https://backendchess.herokuapp.com";
+
+const socket = io(URL);
+
+var mySocketId;
+// register preliminary event listeners here:
+
+socket.on("createNewGame", async (statusUpdate) => {
+  console.log(
+    "A new game has been created! Username: " +
+      statusUpdate.userName +
+      ", Game id: " +
+      statusUpdate.gameId +
+      " Socket id: " +
+      statusUpdate.mySocketId
+  );
+  mySocketId = await statusUpdate.mySocketId;
+});
+
+export { socket, mySocketId };
