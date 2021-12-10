80f9ca1bc47a42c2eec842e6ed608b713b681564
ParthGada <parthrgada@gmail.com>
2021-12-10 13:43:31 +0530

    Update bettingAmount.js

--- src/components/Crypto/smartContract/bettingAmount.js
+++ src/components/Crypto/smartContract/bettingAmount.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { useMoralis, useNativeBalance } from "react-moralis";
 import { useSmartContract } from "hooks/useSmartContract";
+import jumpchess from "img/jumpchess.gif";
 
 function BettingAmount() {
   const [bet, setBet] = useState(0);
@@ -18,13 +19,14 @@ function BettingAmount() {
 
   return (
     <div>
-      <h1>Betting Amount</h1>
+      <h1 style={{paddingBottom:"50px"}}>Enter $MATIC You Want to Stake/Bet</h1>
       <input
         type="number"
         value={bet}
         onChange={(e) => setBet(e.target.value)}
+        style={{marginLeft:"33%",marginRight:"10px", backgroundColor: "white", fontSize:"18px", borderRadius:"5px", padding:"10px"}}
       />
-      <button
+      <button style={{borderRadius:"5px", padding:"10px", fontSize:"18px"}}
         onClick={() => {
           if (bet > 0 && bet < balance.formatted) {
             initGameWhite(bet).then((res) => {
@@ -35,8 +37,10 @@ function BettingAmount() {
           }
         }}
       >
-        Bet
+        Continue
       </button>
+      <img src={jumpchess} style={{paddingLeft:"40%", marginTop: "80px", marginBottom:"10px", height:"240px", width:"auto"}} />
+
     </div>
   );
 }
